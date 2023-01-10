e9140f2cd52453d19170f93be1cfd63fab81dbf9
Facundo-p1 <facundoponce715@gmail.com>
2023-01-10 10:56:39 -0300

    Uso de js

---
+++ index.js
@@ -0,0 +1,20 @@
+for(let i = 0; i <1; i++){
+
+    const usuario = prompt ( "Ingrese Su Nombre")
+
+    const mail = prompt ( "Ingrese Su Mail")
+
+    const direccion  = prompt ( "Ingrese Su Direccion")
+
+    const telefono = prompt ( "Ingrese Su Telfono")
+
+
+   }
+
+   do{
+    
+    edad = parseInt (prompt ("Ingrese Su Edad"))
+
+    alert ("Ingresaste una edad menor a 18, unicamente se puede acceder siendo mayores de edad")
+    
+   } while (edad < 18)
