354878a1acd19ce800133e775698aaa31d5612cb
Facundo-p1 <facundoponce715@gmail.com>
2023-02-07 20:35:50 -0300

    Aplicando js?

--- main.js
+++ main.js
@@ -1,43 +1,71 @@
-//Funciones
+localStorage.setItem("nombre", "Facundo");
 
-function saludoinicial(){
-   document.write ("Bienvenidos a yasbakery");
+ const nombreUsuario = localStorage.getItem("nombre");
+ console.log(nombreUsuario);
+
+
+
+   //Ejecutando funciones
+document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
+document.getElementById("btn__registrarse").addEventListener("click", register);
+window.addEventListener("resize", anchoPage);
+
+//Declarando variables
+var formulario_login = document.querySelector(".formulario__login");
+var formulario_register = document.querySelector(".formulario__register");
+var contenedor_login_register = document.querySelector(".contenedor__login-register");
+var caja_trasera_login = document.querySelector(".caja__trasera-login");
+var caja_trasera_register = document.querySelector(".caja__trasera-register");
+
+    //FUNCIONES
+
+function anchoPage(){
+
+    if (window.innerWidth > 850){
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "block";
+    }else{
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_register.style.display = "none";   
+    }
 }
 
-saludoinicial();
+anchoPage();
 
-//For
 
-for(let i = 0; i < 1; i++) {
+    function iniciarSesion(){
+        if (window.innerWidth > 850){
+            formulario_login.style.display = "block";
+            contenedor_login_register.style.left = "10px";
+            formulario_register.style.display = "none";
+            caja_trasera_register.style.opacity = "1";
+            caja_trasera_login.style.opacity = "0";
+        }else{
+            formulario_login.style.display = "block";
+            contenedor_login_register.style.left = "0px";
+            formulario_register.style.display = "none";
+            caja_trasera_register.style.display = "block";
+            caja_trasera_login.style.display = "none";
+        }
+    }
 
-   const nombre = parseInt(prompt("Ingrese su nombre"));
-
-   const edad = parseInt(prompt("Ingrese la edad"));
-   
-   if(edad < 18) {
-   
-   alert("Se ingresó una edad menor a 18");
-   
-   break; }
-   }
-
-//Arrays
-
-const nombres = ["Malena", "Juana", "Maria"]
-
-console.log (nombres)
-
-const edades =["19", "30", "50"]
-
-console.log(edades)
-
-//objetos   
-
-   let persona = {
-    nombre:'Juan',
-    apellido: 'Arroyo',
-    edad:31,
-
-   }
-   console.log(persona)
-
+    function register(){
+        if (window.innerWidth > 850){
+            formulario_register.style.display = "block";
+            contenedor_login_register.style.left = "410px";
+            formulario_login.style.display = "none";
+            caja_trasera_register.style.opacity = "0";
+            caja_trasera_login.style.opacity = "1";
+        }else{
+            formulario_register.style.display = "block";
+            contenedor_login_register.style.left = "0px";
+            formulario_login.style.display = "none";
+            caja_trasera_register.style.display = "none";
+            caja_trasera_login.style.display = "block";
+            caja_trasera_login.style.opacity = "1";
+        }
+    }
